8298879d01c687b94ea31fdc4ba3621d0537ce0c
Sowndaryakp <sonalshetty.1999@gmail.com>
2024-03-17 22:51:36 +0530

    pattern problem

--- Strivers-AtoZ-DSA/Challenges/pattern11.java
+++ Strivers-AtoZ-DSA/Challenges/pattern11.java
@@ -1,3 +1,19 @@
 public class pattern11 {
-    
+    static void printPattern11(int N){
+        int start = 1;
+        for(int i=0;i<N;i++){
+            if(i%2 == 0) start = 1;
+            else start = 0;
+
+            for(int j=0;j<=i;j++){
+                System.out.print(start);
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int N = 5;
+        printPattern11(N);
+    }
 }
